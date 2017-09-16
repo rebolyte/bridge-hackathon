2269f55111f3cd077fbab49767243f1d9f20b712
Peter Weyand <pweyand@gmail.com>
2017-09-15 22:37:48 -0500

    fixed app margin top

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
   display: grid;
   grid-template-columns: 15vw 85vw;
   grid-template-rows: 10vh 90vh;
